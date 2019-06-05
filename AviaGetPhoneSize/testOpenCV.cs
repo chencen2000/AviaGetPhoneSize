4d124ac04ff0b89e0d08f0462119dcd1a277c9c5
chencen2000 <chencen2000@gmail.com>
2019-06-04 21:53:20 -0700

    test

--- AviaGetPhoneSize/testOpenCV.cs
+++ AviaGetPhoneSize/testOpenCV.cs
@@ -11,23 +11,47 @@ using System.Threading.Tasks;
 
 namespace AviaGetPhoneSize
 {
+    class myContour
+    {
+        public Point []points;
+        public int index;
+        public double area;
+        public int next;
+        public int previous;
+        public int child;
+        public int parent;
+        public myContour(VectorOfPoint pnts, int index, double area, int next, int previous, int child, int parent)
+        {
+            points = pnts.ToArray();
+            this.index = index;
+            this.area = area;
+            this.next = next;
+            this.previous = previous;
+            this.child = child;
+            this.parent = parent;
+        }
+    }
+
     class testOpenCV
     {
         static int Main(string[] args)
         {
-            test_2();
+            //test();
+            //test_2();
+            //test_3();
+            test_4();
             return 0;
         }
         static void test()
         {
             //string fn = @"C:\Tools\avia\images\Final270\iphone6 Gold\0123.1.bmp";
-            string fn = @"C:\Tools\avia\images\Final270\iphone6s Plus Silver\1471.1.bmp";
+            string fn = @"C:\Tools\avia\Recog source\AP001-iphone6_gold\0123.1.bmp";
             //string fn = @"temp_1.jpg";
             Mat m = CvInvoke.Imread(fn);
             Image<Gray, Byte> img = m.ToImage<Gray, Byte>().GetSubRect(new Rectangle(new Point(5, 5), new Size(m.Width - 10, m.Height - 10))).Resize(0.1, Inter.Cubic);
 
 
-            CvInvoke.GaussianBlur(img, img, new Size(5, 5), 0);
+            CvInvoke.GaussianBlur(img, img, new Size(3, 3), 0);
             double otsu = CvInvoke.Threshold(img, new Mat(), 0, 255, ThresholdType.Binary | ThresholdType.Otsu);
             double sigma = 0.25;
             double lower = Math.Max(1, (1.0 - sigma) * otsu);
@@ -56,7 +80,7 @@ namespace AviaGetPhoneSize
         {
             Mat m = CvInvoke.Imread("temp_2.jpg");
             Image<Gray, byte> img = m.ToImage<Gray, byte>();
-            List<Tuple<int, VectorOfPoint, int, int, int, int>> data = new List<Tuple<int, VectorOfPoint, int, int, int, int>>();
+            List<myContour> myc = new List<myContour>();
             using (VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint())
             {
                 Mat info = new Mat();
@@ -72,21 +96,34 @@ namespace AviaGetPhoneSize
                     int previous = infox.Data[0, i * 4+1];
                     int child = infox.Data[0, i * 4+2];
                     int parent = infox.Data[0, i * 4+3];
-                    Tuple<int, VectorOfPoint, int, int, int, int> r = new Tuple<int, VectorOfPoint, int, int, int, int>(i, contour, next, previous, child, parent);
-                    if(parent!=-1 && parent >= i)
-                    {
-
-                    }
-                    data.Add(r);
+                    myContour c = new myContour(contour, i, a, next, previous, child, parent);
+                    myc.Add(c);
                 }
             }
-            List<Tuple<int, VectorOfPoint, int, int, int, int>> outer = new List<Tuple<int, VectorOfPoint, int, int, int, int>>();
-            foreach (var v in data)
+            List<myContour> outer = new List<myContour>();
+            foreach (myContour c in myc)
             {
-                if(v.Item6==-1 && v.Item5 != -1)
+                if(c.area>1000.0 && c.child!=-1 && c.parent == -1)
                 {
+                    outer.Add(c);
                 }
             }
+            //VectorOfVectorOfPoint vvp = new VectorOfVectorOfPoint();
+            VectorOfPoint match_t = test_3();
+            foreach (myContour c in outer)
+            {
+                //using (VectorOfVectorOfPoint vvp = new VectorOfVectorOfPoint())
+                //{
+                //    VectorOfPoint vp = new VectorOfPoint(c.points);
+                //    vvp.Push(vp);
+                //    CvInvoke.DrawContours(m, vvp, -1, new MCvScalar(0, 255, 0));
+                //    m.Save("temp_3.jpg");
+                //}
+                //CvInvoke.DrawContours(m, vp, 0, new MCvScalar(0, 255, 0));
+                double d = CvInvoke.MatchShapes(match_t, new VectorOfPoint(c.points), ContoursMatchType.I1);
+            }
+            
+            m.Save("temp_3.jpg");
         }
         static Dictionary<int,object> find_contour(Dictionary<int,object> data, int id)
         {
@@ -107,6 +144,53 @@ namespace AviaGetPhoneSize
                 }
             }
             return ret;
+        }
+        static VectorOfPoint test_3()
+        {
+            Mat m = CvInvoke.Imread(@"C:\Tools\avia\images\Apple_logo_1.png");
+            CvInvoke.GaussianBlur(m, m, new Size(5, 5), 0);
+            Image<Gray, Byte> img = m.ToImage<Gray, Byte>();
+            CvInvoke.Threshold(img, img, 0, 255, ThresholdType.Binary | ThresholdType.Otsu);
+            VectorOfVectorOfPoint vvp1 = new VectorOfVectorOfPoint();
+            CvInvoke.FindContours(img, vvp1, null, RetrType.External, ChainApproxMethod.ChainApproxNone);
+            int cnt = vvp1.Size;
+            CvInvoke.DrawContours(m, vvp1, -1, new MCvScalar(0, 255, 0), 2);
+            m.Save("temp_3.jpg");
+            //VectorOfVectorOfPoint vvp0 = new VectorOfVectorOfPoint();
+            VectorOfPoint all_vp = new VectorOfPoint();
+            for (int i=0; i<cnt; i++)
+            {
+                VectorOfPoint vp = vvp1[i];
+                all_vp.Push(vp);
+            }
+            //return vvp1[1];
+            return all_vp;
+        }
+        static void test_4()
+        {
+            Mat m = CvInvoke.Imread("temp_1.jpg");
+            Image<Gray, byte> img = m.ToImage<Gray, byte>();
+            List<myContour> myc = new List<myContour>();
+            using (VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint())
+            {
+                VectorOfPoint t = test_3();
+                CvInvoke.FindContours(img, contours, null, RetrType.List, ChainApproxMethod.ChainApproxNone);
+                int count = contours.Size;
+                for (int i = 0; i < count; i++)
+                {
+                    VectorOfPoint contour = contours[i];
+                    double a = CvInvoke.ContourArea(contour);
+                    if (a > 100.0)
+                    {
+                        double d = CvInvoke.MatchShapes(t, contour, ContoursMatchType.I1);
+                        if (d < 0.05)
+                        {
+                            Program.logIt($"match: {d}");
+                        }
+                    }
+                }
+            }
+
         }
     }
 }
