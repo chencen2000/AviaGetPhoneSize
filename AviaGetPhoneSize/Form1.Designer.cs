78fbc435ab549adfc303fcd0dcba8410e4ffeffc
Chris Chen <cchen@futuredial.com>
2019-08-09 14:09:11 -0700

    test

--- AviaGetPhoneSize/Form1.Designer.cs
+++ AviaGetPhoneSize/Form1.Designer.cs
@@ -34,6 +34,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.textBoxComment = new System.Windows.Forms.TextBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panelLeft = new System.Windows.Forms.Panel();
             this.panel1.SuspendLayout();
             this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -46,7 +47,7 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(800, 39);
+            this.panel1.Size = new System.Drawing.Size(969, 39);
             this.panel1.TabIndex = 0;
             this.panel1.Visible = false;
             // 
@@ -64,7 +65,7 @@
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 1;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 39);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(969, 39);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // label1
@@ -81,7 +82,7 @@
             // button1
             // 
             this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button1.Location = new System.Drawing.Point(722, 3);
+            this.button1.Location = new System.Drawing.Point(891, 3);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 33);
             this.button1.TabIndex = 2;
@@ -94,7 +95,7 @@
             this.textBoxComment.Dock = System.Windows.Forms.DockStyle.Fill;
             this.textBoxComment.Location = new System.Drawing.Point(58, 3);
             this.textBoxComment.Name = "textBoxComment";
-            this.textBoxComment.Size = new System.Drawing.Size(658, 20);
+            this.textBoxComment.Size = new System.Drawing.Size(827, 20);
             this.textBoxComment.TabIndex = 1;
             // 
             // pictureBox1
@@ -102,16 +103,26 @@
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pictureBox1.Location = new System.Drawing.Point(0, 39);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(800, 411);
+            this.pictureBox1.Size = new System.Drawing.Size(969, 607);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.DoubleClick += new System.EventHandler(this.PictureBox1_DoubleClick);
+            // 
+            // panelLeft
+            // 
+            this.panelLeft.BackColor = System.Drawing.SystemColors.Info;
+            this.panelLeft.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelLeft.Location = new System.Drawing.Point(0, 39);
+            this.panelLeft.Name = "panelLeft";
+            this.panelLeft.Size = new System.Drawing.Size(253, 607);
+            this.panelLeft.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(969, 646);
+            this.Controls.Add(this.panelLeft);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
@@ -133,5 +144,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBoxComment;
+        private System.Windows.Forms.Panel panelLeft;
     }
 }
